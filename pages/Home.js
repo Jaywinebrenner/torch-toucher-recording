c286add4438f12925639ef638f6039d97800e0a7
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-04-26 15:45:34 -0700

    wire up html

---
+++ pages/Home.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className='home__nav'>
+
+      </div>
+      <div className='home__body'>
+        
+      </div>
+      <div className='home__footer'>
+
+      </div>
+     
+    </div>
+  )
+}
