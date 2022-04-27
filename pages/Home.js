5c18440112a4c51ae69c2e0916bc88c53df283c7
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-04-27 11:43:28 -0700

    deployment test

--- pages/Home.js
+++ pages/Home.js
@@ -3,7 +3,7 @@ import React, {useState, useRef, useEffect} from 'react'
 import { useRouter } from 'next/router'
 
 
-export default function Home() {
+export default function home() {
 
   const photos = [
     {
